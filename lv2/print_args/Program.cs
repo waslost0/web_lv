8b54674f235e539e0e561a4faf941b20e03ffc1c
Mark <marcus.koh16.01.97@gmail.com>
2020-03-19 16:48:59 +0300

    lv2 upload

---
+++ lv2/print_args/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace print_args
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("No parameters were specified!");
+                Environment.Exit(1);
+            }
+
+            Console.WriteLine("Number of arguments: {0}", args.Length);
+            Console.WriteLine("Arguments: {0}", string.Join(" ", args));
+
+        }
+    }
+}
---
+++ lv2/remove_duplicates/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace remove_duplicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 1 || args.Length < 1)
+            {
+                Console.WriteLine("Incorrect number of arguments!\nUsage remove_duplicates.exe <input string>");
+                Environment.Exit(1);
+            }
+            
+            Console.WriteLine(args[0].Distinct().ToArray());
+
+        }
+    }
+}
