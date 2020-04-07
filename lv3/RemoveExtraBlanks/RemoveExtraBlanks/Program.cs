eb5f105081507f9acd612deb372ebe83f2e1ec06
Mark <marcus.koh16.01.97@gmail.com>
2020-04-07 20:42:57 +0300

    add tests RemoveExtraBlanks, add main program RemoveExtraBlanks

---
+++ lv3/RemoveExtraBlanks/RemoveExtraBlanks/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace RemoveExtraBlanks
+{
+    public static class Program
+    { 
+        public static void Main(string[] args)
+        {
+            if (!ParseArgs(args))
+            {
+                Console.WriteLine("Invalid arguments count.\nUsage RemoveExtraBlanks.exe <inputFile> <outputFile>");
+                return;
+            }
+
+            string inputFile = args[0];
+            string outputFile = args[1];
+
+
+            if (!CheckFiles(inputFile, outputFile)) return;
+
+            string line;
+
+            using (StreamReader reader = new StreamReader(inputFile))
+            using (StreamWriter writer = new StreamWriter(outputFile))
+            {
+                while ((line = reader.ReadLine()) != null)
+                {
+                    line = TrimAndRemoveExtraBlanks(line);
+                    writer.WriteLine(line);
+                }
+            }
+        }
+
+        public static string TrimAndRemoveExtraBlanks(string inputString)
+        {
+            string result= TrimString(inputString);
+            result = RemoveExtraBlanks(result);
+            return result;
+        }
+
+        public static string TrimString(string inputString, char[] charsToTrim = null)
+        {
+            return inputString.Trim(charsToTrim);
+        }
+
+        public static string RemoveExtraBlanks(string inputString)
+        {
+            string pattern = @"[ \t]+";
+            string replacement = " ";
+            return Regex.Replace(inputString, pattern, replacement);
+        }
+
+        public static bool CheckFiles(string inputFile, string outputFile)
+        {
+            if (inputFile == outputFile)
+            {
+                Console.WriteLine("Files cannot be equal.");
+                return false;
+            }
+
+            if (!File.Exists(inputFile))
+            {
+                Console.WriteLine($"File {inputFile} does not exist.");
+                return false;
+            }
+            else if (!File.Exists(outputFile))
+            {
+                using (File.CreateText(outputFile)) { }
+                Console.WriteLine($"File {outputFile} does not exist. Was created.");
+                return true;
+
+            }
+            return true;
+        }
+
+        public static bool ParseArgs(string[] args)
+        {
+            if (args.Length != 2)
+                return false;
+            return true;
+        }
+    }
+}
---
+++ lv3/RemoveExtraBlanks/RemoveExtraBlanksTests/UnitTest1.cs
@@ -0,0 +1,183 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RemoveExtraBlanks;
+using System.IO;
+
+namespace RemoveExtraBlanksTests
+{
+    [TestClass]
+    public class ParseArgsTest
+    {
+
+        [TestMethod]
+        public void InvalidArgumentsCount()
+        {
+            string[] stringArray = { "input.txt", "o.txt" };
+
+            bool result = Program.ParseArgs(stringArray);
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void TooFewArguments()
+        {
+            string[] stringArray = { "input.txt", "o.txt" , "string"};
+
+            bool result = Program.ParseArgs(stringArray);
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void ValidArgumentsCount()
+        {
+            string[] stringArray = { "input.txt" };
+
+            bool result = Program.ParseArgs(stringArray);
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void EmptyArguments()
+        {
+            string[] stringArray = System.Array.Empty<string>();
+
+            bool result = Program.ParseArgs(stringArray);
+            Assert.IsFalse(result);
+        }
+
+
+    }
+
+    [TestClass]
+    public class FilesTest
+    {
+        private const string inputFileName = "input.txt";
+        private const string outputFileName = "output.txt";
+
+        [TestMethod]
+        public void BothFilesExist()
+        {
+            using (File.CreateText(inputFileName)) { }
+
+            bool result = Program.CheckFiles(inputFileName, outputFileName);
+            Assert.IsTrue(result);
+            File.Delete(inputFileName);
+            File.Delete(outputFileName);
+        }
+
+
+        [TestMethod]
+        public void OutputFileDoesNotExist()
+        {
+            using (File.CreateText(inputFileName)) { }
+
+            bool result = Program.CheckFiles(inputFileName, outputFileName);
+            Assert.IsTrue(result);
+            Assert.IsTrue(File.Exists(outputFileName));
+
+            File.Delete(inputFileName);
+            File.Delete(outputFileName);
+        }
+
+
+        [TestMethod]
+        public void InputFileDoesNotExist()
+        {
+            bool result = Program.CheckFiles(inputFileName, outputFileName);
+            Assert.IsFalse(result);
+        }
+
+
+        [TestMethod]
+        public void FileNamesAreEqual()
+        {
+            bool result = Program.CheckFiles(inputFileName, inputFileName);
+            Assert.IsFalse(result);
+        }
+    }
+
+
+    [TestClass]
+    public class TrimStringTest
+    {
+
+        [TestMethod]
+        public void SpaceOnStartAndEnd()
+        {
+            string stringToTrim = " Hello, there ";
+
+            Assert.AreEqual("Hello, there", Program.TrimString(stringToTrim));
+        }
+
+        [TestMethod]
+        public void TabOnStartAndEnd()
+        {
+            string stringToTrim = "\tHello, there\t";
+
+            Assert.AreEqual("Hello, there", Program.TrimString(stringToTrim));
+        }
+        [TestMethod]
+        public void MixOfTabsAndSapcesOnStartAndEndOfString()
+        {
+            string stringToTrim = "\t \t     \tHello, there\t   \t\t  ";
+
+            Assert.AreEqual("Hello, there", Program.TrimString(stringToTrim));
+        }
+        [TestMethod]
+        public void EmptyString()
+        {
+            string stringToTrim = "";
+
+            Assert.AreEqual("", Program.TrimString(stringToTrim));
+        }
+        [TestMethod]
+        public void StringConsistTabs()
+        {
+            string stringToTrim = "\t\t\t";
+
+            Assert.AreEqual("", Program.TrimString(stringToTrim));
+        }
+    }
+
+
+    [TestClass]
+    public class RemoveExtraBlanksTest
+    {
+        [TestMethod]
+        public void EmptyString()
+        {
+            string inputString = "";
+            Assert.AreEqual("", Program.RemoveExtraBlanks(inputString));
+        }
+
+
+        [TestMethod]
+        public void InputStringIsOneSpace()
+        {
+            string inputString = " ";
+            Assert.AreEqual(" ", Program.RemoveExtraBlanks(inputString));
+        }
+
+        [TestMethod]
+        public void MultipleLinesStringAndMixOfSpacesAndTabs()
+        {
+            string inputString = " String   with         \nmultiple            lines ";
+            Assert.AreEqual(" String with \nmultiple lines ", Program.RemoveExtraBlanks(inputString));
+        }
+
+        [TestMethod]
+        public void NormalStringWithoutDuplicatesOfSpacesAndTabs()
+        {
+            string inputString = "Just noraml string.";
+            Assert.AreEqual("Just noraml string.", Program.RemoveExtraBlanks(inputString));
+        }
+
+        [TestMethod]
+        public void NormalSngWithoutDuplicatesOfSpacesAndTabs()
+        {
+            string inputString = "Just noraml string.";
+            Assert.AreEqual("Just noraml string.", Program.RemoveExtraBlanks(inputString));
+        }
+
+
+    }
+}
